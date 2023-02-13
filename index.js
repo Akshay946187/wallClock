b3c55f041f1cfdf11d65ab9eb3d17bca0f288120
Akshay946187 <aki748434@gmail.com>
2023-02-13 11:36:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    hr = 30*htime + mtime/2;
+    mr = 6*mtime;
+    sr = 6*stime;
+
+    hour.style.transform = `rotate(${hr}deg)`;
+    minut.style.transform = `rotate(${mr}deg)`;
+    second.style.transform = `rotate(${sr}deg)`;
+},1000)
